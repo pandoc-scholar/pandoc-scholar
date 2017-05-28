aa2e7c9939cad796b60685b62c186a341d0120d0
Albert Krewinkel <albert@zeitkraut.de>
2017-05-28 21:58:59 +0200

    Add author info as first block to docx, epub, odt

--- writers/default.lua
+++ writers/default.lua
@@ -21,18 +21,78 @@ function Doc(body, meta, variables)
   local authors, affiliations =
     scholarlymeta.canonicalize_authors(meta.author, meta.institute)
   meta.author = {}
+
+  local author_notes = {}
+  for i = 0, #authors do
+    author_notes[i] = {}
+  end
+
+  -- Corresponding authors
+  local corresponding_authors = {}
   for k, author in ipairs(authors) do
-    if author.institute and author.institute[1] then
-      local inst = panlunatic.Space()
-        .. panlunatic.Str('(')
-        .. author.institute[1].name
-        .. panlunatic.Str(')')
-      meta.author[k] = author.name .. inst
-    else
-      meta.author[k] = author.name
+    if author.correspondence and author.email then
+      local mailto = "mailto:" .. panlunatic.decode(author.email).c
+      local envelope = panlunatic.Superscript(panlunatic.Str "✉")
+      local attr = {id = "", class = ""}
+      local link = panlunatic.Link(envelope, mailto, "", attr)
+      local author_with_mail =
+        author.name .. panlunatic.Space() .. panlunatic.Str "<" ..
+        author.email .. panlunatic.Str ">"
+      table.insert(author_notes[k], panlunatic.Link(envelope, mailto, "", attr))
+      table.insert(
+        corresponding_authors,
+        panlunatic.Link(author_with_mail, mailto, "", attr)
+      )
     end
   end
-  meta.institute = affiliations:map(function(inst) return inst.name end)
+  if #corresponding_authors > 0 then
+    local correspondence = panlunatic.Str "Correspondence:" .. panlunatic.Space()
+    local sep = panlunatic.Str "," .. panlunatic.Space()
+    body = panlunatic.Para(correspondence .. table.concat(corresponding_authors, sep)) ..
+      ',' .. body
+  end
+
+  -- Equal contributions
+  local has_equal_contributors = false
+  for k, author in ipairs(authors) do
+    if author.equal_contributor then
+      has_equal_contributors = true
+      table.insert(author_notes[k], panlunatic.Str "*")
+    end
+  end
+  if has_equal_contributors then
+    local contributors =
+      panlunatic.Superscript(panlunatic.Str "*") ..
+      panlunatic.Space() ..
+      panlunatic.Str "These authors contributed equally to this work."
+    body = panlunatic.Para(contributors) .. panlunatic.Blocksep() .. body
+  end
+
+  for k, author in ipairs(authors) do
+    for _, idx in ipairs(author.institute_indices) do
+      table.insert(author_notes[k], panlunatic.Str(tostring(idx)))
+    end
+  end
+  for k, author in ipairs(authors) do
+    meta.author[k] = author.name ..
+      panlunatic.Superscript(table.concat(author_notes[k], panlunatic.Str ","))
+  end
+
+  -- meta.institute = affiliations:map(function(inst) return inst.name end)
+  local affil_blocks = {}
+  for i, affil in ipairs(affiliations) do
+    table.insert(affil_blocks,
+      panlunatic.Superscript(panlunatic.Str(tostring(i))) ..
+      panlunatic.Space() ..
+      affil.name
+    )
+  end
+  io.stderr:write(tostring(#affiliations))
+  io.stderr:write(tostring(#affil_blocks))
+
+  body = panlunatic.Para(table.concat(affil_blocks, panlunatic.LineBreak())) ..
+    panlunatic.Blocksep() .. body
+
   return panlunatic.Doc(body, meta, variables)
 end
 
